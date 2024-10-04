3776a6e3fe6149fae80c8713dddadc34ffed7b6c
Nurcan Ceferli <nurcanceferli04@gmail.com>
2024-10-04 15:15:29 +0300

    Updates

---
+++ static/js/952.aaaf0c30.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfinal_project=self.webpackChunkfinal_project||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.aaaf0c30.chunk.js.map
